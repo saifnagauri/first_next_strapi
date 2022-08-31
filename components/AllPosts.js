509780ee4d4126c79f0580d6317ce33cab73efec
saifnagauri <developer.saifnagauri@gmail.com>
2022-08-31 22:51:21 +0530

    second commit

--- components/AllPosts.js
+++ components/AllPosts.js
@@ -2,8 +2,6 @@ import React from "react";
 import PostPreview from "./PostPreview";
 
 const AllPosts = ({ posts }) => {
-  //console.log('posts page ',posts)
-
   function renderPostPreviews() {
     return posts.map((post) => {
       return <PostPreview post={post} key={post.id} />;
